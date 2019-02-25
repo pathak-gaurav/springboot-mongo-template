524e928ef76a19ca4ae2d497c8b19deb0a3550cc
gauravpathak <gauravpathak.spring@gmail.com>
2019-02-25 22:11:09 +0530

    Initial commit

---
+++ src/main/java/com/gaurav/SpringbootMongoTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.gaurav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMongoTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMongoTemplateApplication.class, args);
+    }
+
+}
